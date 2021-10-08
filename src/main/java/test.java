607fc02c417f458060eed755658b0d59fc60f3a8
czy <2524134864@qq.com>
2021-10-08 17:44:29 +0800

    3

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,6 +1,7 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("git成功");
-        System.out.println("3333");
+        System.out.println("2222");
+        System.out.println("登录口令 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");
     }
 }
