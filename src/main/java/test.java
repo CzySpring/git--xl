51d82c19612618e5d716101f96a55ed23e989ae5
CzySpring <92147399+CzySpring@users.noreply.github.com>
2021-10-10 17:09:06 +0800

    Update test.java

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -3,6 +3,6 @@ public class test {
         System.out.println("git成功");
         System.out.println("2222");
         System.out.println("登录口令 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");
-        System.out.println("骚逼母狗唐成姣");
+       
     }
 }
